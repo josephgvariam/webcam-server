673503a23db8071612dad9852768519d2e68f818
Joseph George Variam <joseph@variam.com>
2014-03-29 02:50:17 +0530

    added static content handler to embedded jetty

--- src/main/java/com/joppu/webcam/server/Main.java
+++ src/main/java/com/joppu/webcam/server/Main.java
@@ -1,6 +1,10 @@
 package com.joppu.webcam.server;
 
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.glassfish.jersey.filter.LoggingFilter;
 import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
@@ -25,6 +29,14 @@ public class Main {
         resourceConfig.register(MultiPartFeature.class);
 
         Server server = JettyHttpContainerFactory.createServer(baseUri, resourceConfig);
+
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setDirectoriesListed(true);
+        resourceHandler.setWelcomeFiles(new String[]{"index.html"});
+        resourceHandler.setResourceBase("app");
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] { resourceHandler, new DefaultHandler() });
+        server.setHandler(handlers);
 
         LOG.log(Level.INFO, "Starting Webcam Server...");
 
